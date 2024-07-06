0311eff20599c40b10eb0de6fbc90a10d25f5409
masataka <m@clipcrow.com>
2024-07-06 16:28:19 +0900

    PoC5

--- serve.ts
+++ serve.ts
@@ -82,7 +82,7 @@ router.post("/webhook", async (context) => {
 });
 
 router.post("/action", async (context) => {
-  console.log(await context.request.body.json());
+  console.log((await context.request.body.formData()).keys);
   context.response.status = 200;
 });
 
