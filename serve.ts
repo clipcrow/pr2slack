7dbdb4a6d66802e30cb8818934b0f1f08a04fe46
masataka <m@clipcrow.com>
2024-07-07 21:12:38 +0900

    payload.view.idを取得

--- serve.ts
+++ serve.ts
@@ -130,11 +130,9 @@ router.post("/action", async (context) => {
       openDialog(slackToken, payload.trigger_id, userAccountMap, false);
       context.response.status = 200;
     } else if (action?.action_id === "delete_account") {
-      console.log(payload.view);
-
       deleteAccountMapping(action.value);
       const userAccountMap = await listAccountMapping();
-      openDialog(slackToken, payload.view.view_id, userAccountMap, true);
+      openDialog(slackToken, payload.view.id, userAccountMap, true);
       context.response.status = 200;
     }
   } else if (payload.type === "view_submission") {
