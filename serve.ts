9e4b03102849b3abd459f350267ca74ad3dd5dcb
masataka <m@clipcrow.com>
2024-07-16 16:28:46 +0900

    リファクタ

--- serve.ts
+++ serve.ts
@@ -3,20 +3,20 @@ import mainRouter from "./mainRouter.ts";
 import supportRouter from "./supportRouter.ts";
 import mappingRouter from "./mappingRouter.ts";
 
-const router = new Router();
 const githubToken = Deno.env.get("GITHUB_TOKEN");
 const slackToken = Deno.env.get("SLACK_TOKEN");
 
 if (githubToken && slackToken) {
+  const router = new Router();
   mainRouter(router, githubToken, slackToken);
   supportRouter(router, githubToken, slackToken);
   mappingRouter(router, slackToken);
+
+  const app = new Application();
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  await app.listen({ port: 8080 });
 } else {
   throw "Env Not Found";
 }
-
-const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: 8080 });
