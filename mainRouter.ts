1cacb1084165f2bfb428e1e4d1c8f17cb2bf887a
masataka <m@clipcrow.com>
2024-07-17 13:12:55 +0900

    KVの処理をatomicにする

--- mainRouter.ts
+++ mainRouter.ts
@@ -1,5 +1,4 @@
 import { Router } from "oak";
-import { delay } from "std/async";
 import { listAccountMapping, listRepositoryMapping } from "./store.ts";
 import createContext from "./createContext.ts";
 import postNotification from "./postNotification.ts";
@@ -7,20 +6,30 @@ import postNotification from "./postNotification.ts";
 const kv = await Deno.openKv();
 
 kv.listenQueue(async (payload) => {
-  const { cx, githubToken, slackToken } = payload;
-  if (cx?.repository?.url && cx.baseRef && githubToken && slackToken) {
-    const repositoryMap = await listRepositoryMapping();
-    const url = `${cx.repository.url}/tree/${cx.baseRef}`;
-    const slackChannel = repositoryMap[url];
-    if (slackChannel) {
-      await postNotification(
-        githubToken,
-        slackToken,
-        slackChannel,
-        await listAccountMapping(),
-        cx,
-      );
-      await delay(1000);
+  if (payload.nonce) {
+    const nonce = await kv.get(["nonces", payload.nonce]);
+    if (nonce.value === null) {
+      return;
+    }
+    const { cx, githubToken, slackToken } = payload;
+    if (cx?.repository?.url && cx.baseRef && githubToken && slackToken) {
+      const repositoryMap = await listRepositoryMapping();
+      const url = `${cx.repository.url}/tree/${cx.baseRef}`;
+      const slackChannel = repositoryMap[url];
+      if (slackChannel) {
+        await postNotification(
+          githubToken,
+          slackToken,
+          slackChannel,
+          await listAccountMapping(),
+          cx,
+        );
+        await kv.atomic()
+          .check({ key: nonce.key, versionstamp: nonce.versionstamp })
+          .delete(nonce.key)
+          .sum(["processed_count"], 1n)
+          .commit();
+      }
     }
   }
 });
@@ -34,7 +43,13 @@ export default function (
     if (context.request.hasBody) {
       const cx = createContext(await context.request.body.json());
       if (cx) {
-        kv.enqueue({ cx, githubToken, slackToken });
+        const nonce = crypto.randomUUID();
+        await kv.atomic()
+          .check({ key: ["nonces", nonce], versionstamp: null })
+          .enqueue({ nonce, cx, githubToken, slackToken })
+          .set(["nonces", nonce], true)
+          .sum(["enqueued_count"], 1n)
+          .commit();
         context.response.status = 200;
       }
     }
